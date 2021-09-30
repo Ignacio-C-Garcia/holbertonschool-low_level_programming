c4af0ee959dae4988f4b171a18171df2f6ec0b76
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-09-30 11:16:45 -0700

    d

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,5 @@
  * Return: Always 1 (Error)
  */
 int main(void)
-{
-fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
-return (1);
-}
+{printf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\in");
+return (1);}
