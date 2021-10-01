31adbfaf7da22022c39595fde60609f0c6cd63cb
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-01 13:58:55 -0700

    d

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -5,29 +5,25 @@
 *Return: - 0
 */
 int main(void)
-{	int a=48; //0
-	int b=49; //1
-	for(;a<=56;) //8
-	{	
-
-			for(;b<=57;b++) //9
-			{
-			putchar(a);
-                 	putchar(b);
-			if (a == 56 && b == 57){
-                 putchar('\n');
-                 }else
-                 {
-                 	putchar(',');
-                 	putchar(' ');
-			}}
-			
-
-		a++;
-		b=a+1;
-		}
-
-	
-
+{int a = 48;
+int b = 49;
+for (; a <= 56;)
+{
+for (; b <= 57; b++)
+{
+putchar(a);
+putchar(b);
+if (a == 56 && b == 57)
+{
+putchar('\n');
+}
+else
+{
+putchar(',');
+putchar(' ');
+}}
+a++;
+b = a + 1;
+}
 	return (0);
 }
