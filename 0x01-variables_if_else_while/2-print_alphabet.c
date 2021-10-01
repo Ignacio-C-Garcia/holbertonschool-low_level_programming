a2a7ebae9cd9fc7cbd8f737c430f5e6371b38499
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-01 10:09:28 -0700

    d

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-  char c;
-  for (c = 'a' ; c <= 'z' ; c++) putchar (c);
-  putchar('\n');
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	putchar (c);
+	putchar('\n');
 	return (0);
 }
