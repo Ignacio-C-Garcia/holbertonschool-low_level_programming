fcb1994d4fd2bcb63be6d28cce37372801903637
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-05 09:59:21 -0700

    fix ejercicio 1 alphabet x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,9 +5,14 @@
 void print_alphabet_x10(void)
 {
 	char c;
+	int i;
 
-	for (c = 'a'; c <= 'z'; c++)
+	for (i = 0; i <= 10; i++)
 	{
-	_putchar(c);
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+	_putchar('\n');
 	}
 }
