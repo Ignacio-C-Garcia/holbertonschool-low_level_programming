0cd6fe80db161912adff011c7f7f0c389d297be6
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-05 12:32:58 -0700

    ejercicio 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - return absolute value of a value
+ * @n: a number
+ * Return: 0
+ */
+int _abs(int n)
+{
+
+	if (n > 0)
+	{
+	_putchar(n);
+	return (0);
+	}
+	else
+	{
+	_putchar(-n);
+	return (0);
+	}
+}
