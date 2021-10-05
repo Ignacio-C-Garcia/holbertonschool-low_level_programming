c2a7c1b726869db5ba10d7d231285bf3ebcda2c0
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-05 06:29:11 -0700

    d

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+*main - entry point imprime en consola _putchar
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	printf("_putchar");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+comenzamos con funciones en c y continuamos con loops
