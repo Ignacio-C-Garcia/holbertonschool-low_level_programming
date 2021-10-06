07c5a0de6d6b5b942a744c738547771e5fa4b30d
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-06 14:32:51 -0700

    ejercicio 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,4 +1,44 @@
 #include "main.h"
+/**
+ * times_table - prints the 9 times table
+ * Return: 0 always
+ */
 void times_table(void)
-{	int i;
+{	int fuera;
+	int dentro;
+
+	for (fuera = 0; fuera <= 9; fuera++)
+	{
+		for (dentro = 0; dentro <= 9; dentro++)
+		{
+			if (fuera * dentro >= 10)
+			{
+				_putchar(fuera * dentro / 10 + '0');
+				_putchar(fuera * dentro % 10 + '0');
+				if (dentro == 9)
+				{
+				_putchar('\n');
+				}
+				else
+				{
+				_putchar(',');
+				_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(fuera * dentro + '0');
+				if (dentro == 9)
+				{
+				_putchar('\n');
+				}
+				else
+				{
+				_putchar(',');
+				_putchar(' ');
+				}
+			}
+		}
+	}
 }
