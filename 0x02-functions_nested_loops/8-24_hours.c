d87ab08a10602fd8f63272c46bc664cb5c00a552
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-06 19:05:55 -0700

    d

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * jack_bauer - akcdkc
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int hora;
+	int minutos;
+
+for (hora = 100; hora <= 123; hora++)
+{
+	for (minutos = 100; minutos <= 159; minutos++)
+	{
+		_putchar((minutos / 10 % 10) + 48);
+		_putchar((minutos / 1 % 10) + 48);
+		_putchar('\n');
+
+	}
+}
+
+}
