95f7ccc6395c7924cd4f414a3e0996efa8fff9c1
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-12 16:24:18 -0700

    6 puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts - a function that prints a string, followed by a new line, to stdout.
+ * @str: a string to display to stdout
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		if (i % 2 == 0)
+		{
+		_putchar(str[i]);
+		}
+	}
+
+	_putchar('\n');
+}
