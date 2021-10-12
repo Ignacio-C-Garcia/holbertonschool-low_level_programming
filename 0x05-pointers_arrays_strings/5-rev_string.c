2fc673959c2e7e412b1eda2b9b3d1a5310197a19
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-12 15:16:33 -0700

    acomodando 5 para el checker

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,8 +1,25 @@
 #include "main.h"
 /**
- * rev_string - prints a string, in reverse, followed by a new line.
- * @s: a string;
- */
+* _strlen - a function that returns the length of a string.
+* @s: a string
+* Return: length of s
+*/
+int _strlen(char *s)
+{
+	int i;
+	int count = 0;
+
+	for (i = 0; *(s + i) != 0; i++)
+	{
+		count++;
+	}
+	return (count);
+}
+/**
+* rev_string - prints a string, in reverse, followed by a new line.
+* @s: a string;
+*/
+
 void rev_string(char *s)
 {
 	int aux;
