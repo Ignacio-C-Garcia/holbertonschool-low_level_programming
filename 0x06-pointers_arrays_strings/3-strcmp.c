74190f3c96f69d4e29ee8ee400cc9d39dbf73b6f
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-15 12:00:45 -0700

    test 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,22 +1,4 @@
 /**
- * last_character - return de lenght of a string including \0
- * @s: a string
- * Return: int
- */
-int last_character(char *s)
-{
-	int count = 0;
-	int i;
-
-	for (i = 0; s[i] != 0; i++)
-	{
-		count++;
-	}
-
-	return (count);
-}
-
-/**
  *_strcmp - concatenates two strings. appens the src to dest
  *@s1: string
  *@s2: string
@@ -24,22 +6,13 @@ int last_character(char *s)
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i;
+	int i = 0;
+	int result;
 
-		for (i = 0; s1[i] != 0 && s2[i] != 0; i++)
-		{
-			if (s1[i] > s2[i])
-			{
-				return (1);
-			}
-			else if (s1[i] < s2[i])
-			{
-				return (-1);
-			}
-			else
-			{
-				return (0);
-			}
-		}
-	
+	for (; s1[i] != '\0' && s2[i] != '\0';)
+	{
+		i++;
+	}
+	result = s1[i] - s2[i];
+	return (result);
 }
