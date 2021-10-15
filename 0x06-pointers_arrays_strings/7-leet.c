df2b2fa386c6df7813d3b25a501431f53cced47c
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-15 15:20:01 -0700

    test 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+/**
+ * leet - a function
+ * @str: string
+ * Return: str
+ */
+char *leet(char *str)
+{
+	char min[] = {'a', 'e', 'o', 't', 'l'};
+	char may[] = {'A', 'E', 'O', 'T', 'L'};
+	int num[] = {4, 3, 0, 7, 1};
+	int i;
+	int x;
+
+	for (i = 0; str[i] != 0; i++)
+	{
+		for (x = 0; x <= 4; x++)
+		{
+			if (str[i] == min[x] || str[i] == may[x])
+			{
+				str[i] = num[x];
+			}
+		}
+	}
+
+	return (str);
+}
