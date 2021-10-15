2abc261dbce37b5a2ba0d00de42b20ee34513bbe
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-15 12:24:20 -0700

    test 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,21 +1,5 @@
 #include "main.h"
 /**
-* _strlen - a function that returns the length of a string.
-* @s: a string
-* Return: length of s
-*/
-int _strlen(char *s)
-{
-	int i;
-	int count = 0;
-
-	for (i = 0; *(s + i) != 0; i++)
-	{
-		count++;
-	}
-	return (count);
-}
-/**
 * reverse_array - reverse an array of int
 * @a: an array of int;
 * @n: an int
@@ -25,7 +9,7 @@ void reverse_array(int *a, int n)
 {
 	int aux;
 	int start = 0;
-	int end = n;
+	int end = n - 1;
 
 	for (; start != end && start - 1 != end ; )
 	{
