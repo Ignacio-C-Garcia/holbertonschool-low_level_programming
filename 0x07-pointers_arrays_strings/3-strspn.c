37b1a43753646383d9216d597f32859177bab341
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-18 11:20:06 -0700

    typofixed strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,18 @@ unsigned int _strspn(char *s, char *accept)
 	int aux = 0;
 	unsigned int count = 0;
 
-	for (index = 0; s[index] != 0 && accept[index] != 0; index++)
+	for (index = 0; s[index] != 0; index++)
 	{
 		for (aux = 0; accept[aux] != 0; aux++)
 		{
-			if (s[index] == accept[index + aux])
+			if (s[index] == accept[aux])
 			{
 				count++;
+				break;
 			}
 		}
 		if (accept[aux] == 0)
-			return (0);
+			return (count);
 	}
 
 	return (count);
