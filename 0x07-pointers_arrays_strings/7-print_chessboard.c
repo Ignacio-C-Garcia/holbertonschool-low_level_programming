4c5bb26e0352af8127bcfc45c9d9c8f7ea39f1f8
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-18 18:49:50 -0700

    chess fixed

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,11 +8,12 @@ void print_chessboard(char (*a)[8])
 	int x;
 	int y;
 
-	for (y = 0; y <= 8; y++)
+	for (y = 0; y < 8; y++)
 	{
-		for (x = 0; x <= 8; x++)
+		for (x = 0; x < 8; x++)
 		{
 			_putchar(a[y][x]);
 		}
+		_putchar('\n');
 	}
 }
