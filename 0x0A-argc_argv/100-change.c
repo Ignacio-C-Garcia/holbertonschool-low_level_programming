9e401ace83cb06d86c62cdf920d5e93ebdd3217f
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-22 14:23:25 -0700

    test

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,15 +19,6 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	for (i = 0; argv[1][i] != '\0'; i++)
-	{
-		if (argv[1][i] < 48 || argv[1][i] > 57)
-		{
-			printf("0\n");
-			return (0);
-		}
-	}
-
 	for (i = 0; i <= 4; i++)
 	{
 
