828544be0326e75ddd75bba223e8c07c9b3a24c8
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-22 10:54:00 -0700

    ejercicio 100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the minimum number of coins
+ * @argc: numbers of arguments (always 1)
+ * @argv: string value of argument
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int array[] = {25, 10, 5, 2, 1};
+	int aux = 0;
+	int counter = 0;
+	int i;
+
+	if (argc - 1 != 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	for (i = 0; i <= 4; i++)
+	{
+
+		for (; aux + array[i] <= atoi(argv[1]);)
+		{
+			aux += array[i];
+			counter++;
+		}
+	}
+	printf("%d\n", counter);
+	return (0);
+}
