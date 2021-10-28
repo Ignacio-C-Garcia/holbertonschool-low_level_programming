d3993f695bc71c03cba1dff066fbc819147d5363
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-10-28 12:37:08 -0700

    ejercicio 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+/**
+ * array_range - allocate memory
+ *@max: number
+ *@min: number
+ *Return: pointer
+ */
+int *array_range(int min, int max)
+{
+	int *aux;
+	int i;
+
+	if (min > max)
+		return (NULL);
+	aux = malloc((max - min) * sizeof(int));
+	if (!aux)
+		return (NULL);
+	for (i = 0; min <= max; min++)
+	{
+		aux[i] = min;
+		i++;
+	}
+	return (aux);
+}
