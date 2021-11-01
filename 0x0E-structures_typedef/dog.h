42a0df3c4634df7ff1831f09b3cfc69ccc5a6ae2
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-01 09:52:59 -0700

    typosfixed and description added

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,14 @@
-#ifdef HEADER_H_
-#define HEADER_H_
-#define struct dog
+#ifndef _HEADER_H_
+#define _HEADER_H_
+/**
+ * struct dog - Short description
+ * @name: name of the dog
+ * @age: number
+ * @owner: name of a person
+ *
+ * Description: Longer description
+ */
+struct dog
 {
 	char *name;
 	float age;
