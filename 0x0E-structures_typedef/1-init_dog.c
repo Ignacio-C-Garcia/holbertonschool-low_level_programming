58cf214c876af543225da948f1033f07c97f94b3
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-01 10:23:36 -0700

    test

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,6 +8,12 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (!name)
+		name = "";
+	if (!age)
+		age = 0;
+	if (!owner)
+		owner = "";
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
