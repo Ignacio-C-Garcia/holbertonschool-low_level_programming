e82751cb37b3206a3d5d022540c61c389911a471
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-04 14:58:27 -0700

    solo salto de linea cuando n == 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,7 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		printf("%d%s", va_arg(ap, int), separator);
 	}
 	if (n != 0)
-	printf("%d\n", va_arg(ap, int));
-
+	printf("%d", va_arg(ap, int));
+printf("\n");
 	va_end(ap);
 }
