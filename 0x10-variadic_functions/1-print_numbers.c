80219aef6bca8ab0eb65c29836e165065f3ecb2c
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-04 14:37:48 -0700

    caso borde if n == 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
  * print_numbers - prints all numbers given as a parameter separated by a str
  * @separator: the str that separates numbers
@@ -8,19 +9,21 @@
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
 	va_list ap;
-	unsigned int i;
+	unsigned int i = 0;
 
 	va_start(ap, n);
 
 	if (separator)
+		separator = "";
+	if (n == 1)
+		i--;
+
+	for (; i < (n - 1); i++)
 	{
-
-		for (i = 0; i < n - 1; i++)
-		{
-			printf("%d%s", va_arg(ap, int), separator);
-		}
-		printf("%d\n", va_arg(ap, int));
+		printf("%d%s", va_arg(ap, int), separator);
 	}
-	va_end(ap);
 
+	printf("%d\n", va_arg(ap, int));
+
+	va_end(ap);
 }
