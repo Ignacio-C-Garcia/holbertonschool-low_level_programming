0c247447e7ef7a622ce37cf7d7412f8490219f80
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-05 06:11:39 -0700

    caso borde if format is NULL

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include "variadic_functions.h"
 /**
  * print_all - a function thath prints all types of var
  * @format: a string of formats
@@ -12,36 +13,38 @@ void print_all(const char * const format, ...)
 	int guarangada;
 
 	va_start(ap, format);
-
-	while (format[aux] != 0)
+	if (format)
 	{
-		guarangada = 0;
-
-		switch (format[aux])
+		while (format[aux] != 0)
 		{
-		case 'c':
-			printf("%c", va_arg(ap, int));
-			break;
-		case 'i':
-			printf("%d", va_arg(ap, int));
-			break;
-		case 'f':
-			printf("%f", va_arg(ap, double));
-			break;
-		case 's':
-			str = va_arg(ap, char *);
-			if (!str)
-				str = "(nil)";
-			printf("%s", str);
-			break;
-		default:
-			guarangada = 1;
-		}
-		if (format[aux + 1] && guarangada == 0)
-			printf(", ");
-		aux++;
+			guarangada = 0;
 
+			switch (format[aux])
+			{
+			case 'c':
+				printf("%c", va_arg(ap, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(ap, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(ap, double));
+				break;
+			case 's':
+				str = va_arg(ap, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+			default:
+				guarangada = 1;
+			}
+			if (format[aux + 1] && guarangada == 0)
+			printf(", ");
+			aux++;
+
+		}
+		printf("\n");
 	}
-	printf("\n");
 	va_end(ap);
 }
