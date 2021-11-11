2f4c7585895440c4a954647198bb2ae9e51e59d9
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-11 10:03:13 -0800

    task 2 add node

--- 0x12-singly_linked_lists/tests/lists.h
+++ 0x12-singly_linked_lists/tests/lists.h
@@ -2,6 +2,7 @@
 #define HEADER_LINKED_LIST_H
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -16,6 +17,7 @@ char *str;
 unsigned int len;
 struct list_s *next;
 } list_t;
+int _strlen(char *);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
