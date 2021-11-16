3c6f14758e363a40dab6364109c64fe4a25abf73
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-16 07:43:21 -0800

    task 9 betty style fixed and new code

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,20 +1,24 @@
 #include "lists.h"
+/**
+ *insert_nodeint_at_index - insert a node in a linked list at index
+ *@head: header of linked list
+ *@idx: index
+ *@n: value (n) of node
+ *Return: adress of the new node
+ */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int i;
-	listint_t *header = *head;
-	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *header;
+	listint_t *new_node;
 
-	if (!new_node)
+	if (!head)
 		return (NULL);
-
-	new_node->n = n;
 
 	if (!*head)
-	{
 		return (NULL);
-		free(new_node);
-	}
+
+	header = *head;
 
 	for (i = 0; i < (idx - 1); i++)
 	{
@@ -27,7 +31,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (NULL);
 		}
 	}
-
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+	{
+		*head = header;
+		return (NULL);
+	}
+	new_node->n = n;
 	new_node->next = (*head)->next;
 	(*head)->next = new_node;
 	*head = header;
