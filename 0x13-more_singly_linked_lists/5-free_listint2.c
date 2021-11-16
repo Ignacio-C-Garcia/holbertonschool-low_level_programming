6fbbe9df59d2bf3334768550034dc526242b4b7e
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-16 05:14:20 -0800

    task 5 optimization

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,13 +8,10 @@ void free_listint2(listint_t **head)
 	listint_t *aux_head = *head;
 
 	if (head)
-	{
 		while (*head)
 		{
 			*head = (*head)->next;
 			free(aux_head);
 			aux_head = *head;
 		}
-		*head = NULL;
-	}
 }
