1f042d7fc0a42863cc84c9f110b8abab8111ccc0
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-22 10:36:51 -0800

    task 1 bordercase

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,8 +9,12 @@ int create_file(const char *filename, char *text_content)
 {
 	int aux, c, w;
 
-	aux = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (!filename)
+		return (-1);
 
+	aux = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (!text_content)
+		return (1);
 	if (aux == -1)
 		return (-1);
 	for (c = 0; text_content[c]; c++)
