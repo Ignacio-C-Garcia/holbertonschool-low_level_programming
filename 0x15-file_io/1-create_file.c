87c47d353ff9f8aa05026b850039a3cf727dcdb8
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-22 10:31:29 -0800

    task 1 with bettystyle

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,4 +1,10 @@
 #include "main.h"
+/**
+ *create_file - create a file
+ *@filename: file name
+ *@text_content: text content
+ *Return: a number
+ */
 int create_file(const char *filename, char *text_content)
 {
 	int aux, c, w;
@@ -13,7 +19,10 @@ int create_file(const char *filename, char *text_content)
 	}
 	w = write(aux, text_content, c);
 	if (w == -1)
+	{
+		close(aux);
 		return (-1);
+	}
 	close(aux);
 	return (1);
 }
