abe4962f92d1af364d1ec89bb32c7e72a2e3a296
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-22 10:27:44 -0800

    task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,19 @@
+#include "main.h"
+int create_file(const char *filename, char *text_content)
+{
+	int aux, c, w;
+
+	aux = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (aux == -1)
+		return (-1);
+	for (c = 0; text_content[c]; c++)
+	{
+
+	}
+	w = write(aux, text_content, c);
+	if (w == -1)
+		return (-1);
+	close(aux);
+	return (1);
+}
