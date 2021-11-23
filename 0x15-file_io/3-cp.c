b8403131a414dc1bc4093568d4f5d60cab83d2da
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2021-11-23 07:48:41 -0800

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,62 +1,72 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include "main.h"
 /**
- * main - copies the content of a file to another file
- * @file_from: file with content to copy
- * @file_to: file to copy data to
- * Return: 0 on succes exit
+ * get_error - prints an error
+ * @error: an int number between 97 and 100
+ * @file_name: name of the files
+ * @fd_value: value of file descriptor when failed
+ */
+void get_error(char error, char **file_name, int fd_value)
+{
+	switch (error)
+	{
+		case 97:
+			dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+			exit(97);
+			break;
+		case 98:
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name[1]);
+			exit(98);
+			break;
+		case 99:
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name[2]);
+			exit(99);
+			break;
+		case 100:
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_value);
+			exit(100);
+			break;
+	}
+}
+/**
+ * main - function
+ * @argc: number of arguments
+ * @argv: value of arguments
+ * Return: 0 or a positive number if an error
  */
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, pos_t, pos_b = 0;
-	char *text_from = NULL, buffer[1024];
+	int file_from, file_to, aux;
+	int file_read, file_write;
+	char buff[1024];
 
 	if (argc != 3)
-	{
-		dprintf(STDOUT_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-//----------------------------file_from--------------------------------------------
+		get_error(97, argv, 0);
+
 	file_from = open(argv[1], O_RDONLY);
+
 	if (file_from == -1)
+		get_error(98, argv, 0);
+
+	file_to = open(argv[2], O_CREAT | O_WRONLY, 0644);
+
+		if (file_to == -1)
+			get_error(99, argv, 0);
+
+	for (file_read = 1; file_read > 0; )
 	{
-		dprintf(STDOUT_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
-//-----------------------------file_to--------------------------------------------------
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (file_to == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
-//--------------------------------------------------------------------------------
-	if (read(file_from, text_from, 1024) == -1)
-	{
-		dprintf(STDOUT_FILENO, "Es aca...Error: Can't read from file %s\n", argv[2]);
-		exit(98);
+		file_read = read(file_from, buff, sizeof(buff));
+		if (file_read == -1)
+			get_error(98, argv, 0);
+
+		file_write = write(file_to, buff, file_read);
+		if (file_write == -1)
+			get_error(99, argv, 0);
 	}
 
-	for (pos_t = 0; text_from[pos_t]; pos_t++)
-	{
-		if (pos_b >= 1024)
-		{
-			if (write(file_to, buffer, pos_b) == -1)
-			{
-				dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-				exit(99);
-			}
-			pos_b = 0;
-			continue;
-		}
-		buffer[pos_b] = text_from[pos_t];
-		pos_b++;
-	}
-	if (write(file_to, buffer, pos_b) == -1)
-	{
-		dprintf(STDOUT_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
+
+
+	close(file_from);
+	close(file_to);
 	return (0);
 }
+
