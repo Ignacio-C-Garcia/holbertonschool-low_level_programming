8657afefeb6e281cee3e8d8c7457354a26a83b16
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-01-08 07:00:05 -0800

    task 8 fixed

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -3,21 +3,33 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
     dlistint_t *h = *head;
     unsigned int i = 0;
-    if (!*head && idx == 0)
+    if (!*head && index == 0)
     {
         return (-1);
     }
-
-    for (i = 0; i < idx; i++)
+    if (*head && index == 0)
     {
-        if (!*h)
+    	*head = (*head)->next;
+	if (*head)
+	{
+		free((*head)->prev);
+		(*head)->prev = NULL;
+	}
+	return (1);
+    }
+
+    for (i = 0; i < index; i++)
+    {
+        if (!*head)
             return (-1);
         else
-            *h = (*h)->next;
+            *head = (*head)->next;
     }
 
     (*head)->prev->next = (*head)->next;
     (*head)->next->prev = (*head)->prev;
     free(*head);
     *head = h;
+
+    return (1);
 }
