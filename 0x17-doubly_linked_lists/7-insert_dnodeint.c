1ed6763946806f22ea5aadb2e89852a7bfc75ddc
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-01-08 13:55:37 -0300

    f

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,5 +1,21 @@
 #include "lists.h"
 /**
+ * dlistint_len - return number of nodes of a linked list
+ * @h: header
+ * Return: a number
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t node_counter = 0;
+
+	while (h)
+	{
+		node_counter++;
+		h = h->next;
+	}
+	return (node_counter);
+}
+/**
  * insert_dnodeint_at_index - add a node at the idx of dll
  * @h: header of dll
  * @idx: index to add new node
