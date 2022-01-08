d3eca4f0413c106f953dcd803ee928d090af2264
Ignacio-C-Garcia <ignacioacgarcia@gmail.com>
2022-01-08 11:18:08 -0300

    tasks from 4 until 7 without checker

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int i = 0;
+
+    for (i = 0; i < index; i++)
+    {
+        if (!head)
+            return (NULL);
+        else:
+            head = head->next;
+    }
+
+    return (head);
+}
---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,33 @@
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+    dlistint_t *head = *h;
+    unsigned int i = 0;
+    dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
+    if (!new_node)
+        return (NULL);
+
+    new_node->n = n;
+    new_node->prev = NULL;
+    new_node->next = NULL;
+
+    if (!*h && idx == 0)
+    {
+        *h = new_node;
+        return (new_node);
+    }
+
+    for (i = 0; i < idx; i++)
+    {
+        if (!*h)
+            return (NULL);
+        else:
+            *h = (*h)->next;
+    }
+    new_node->prev = (*h)->prev;
+    new_node->next = *h;
+
+    (*h)->prev->next = new_node;
+    (*h)->prev = new_node;
+    *h = head;
+}
