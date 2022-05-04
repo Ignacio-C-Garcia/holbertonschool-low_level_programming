3a64030ea9aea5e284799136a29b6b0d3a6bcb0e
Ignacio Castellan <ignacioacgarcia@gmail.com>
2022-05-04 06:51:39 -0700

    task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "search_algos.h"
+/**
+ * binary_search - binary_search
+ * @array: array
+ * @size: size
+ * @value: value to search
+ * Return: index where value is located or -1
+ */
+int binary_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+		return (-1);
+	size_t mid;
+	size_t i;
+	size_t low = 0;
+	size_t high = size - 1;
+	char *comma = "";
+
+	while (low <= high)
+	{
+		printf("Searching in array: ");
+		comma = "";
+		for (i = low; i <= high; i++)
+		{
+			printf("%s%d", comma, array[i]);
+			comma = ", ";
+		}
+		printf("\n");
+		mid = low + (high - low) / 2;
+
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			low = mid + 1;
+		else
+			high = mid - 1;
+
+	}
+	return (-1);
+}
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,4 +2,5 @@
 #define algorithms
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+size_t len(int array[]);
 #endif
