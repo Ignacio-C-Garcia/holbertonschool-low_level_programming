94e1018cfc29a527d28824961060eb61a8a18c33
Ignacio Castellan <ignacioacgarcia@gmail.com>
2022-05-04 06:53:50 -0700

    task 1

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -9,14 +9,14 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	if (array == NULL)
-		return (-1);
 	size_t mid;
 	size_t i;
 	size_t low = 0;
 	size_t high = size - 1;
 	char *comma = "";
 
+	if (array == NULL)
+		return (-1);
 	while (low <= high)
 	{
 		printf("Searching in array: ");
